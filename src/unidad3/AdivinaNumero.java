f4d8a9fbaa3c08a9e7853fd672b3a62b33327c6f
Andrea <andreagutierrezvega@gmail.com>
2019-11-21 19:58:55 +0100

    Ejercicio 7 de la unidad 3 resuelto

---
+++ src/unidad3/AdivinaNumero.java
@@ -0,0 +1,27 @@
+package unidad3;
+
+import java.util.Scanner;
+
+public class AdivinaNumero {
+
+	public static void main(String[] args) {
+		int N = (int) Math.floor(Math.random()*(100000-1000+1)+1000);
+		int numeroAdivinar = (int) (Math.random()* N + 1);
+		int respuesta;
+		Scanner scan = new Scanner(System.in);
+		
+		do {
+			System.out.println("He pensado un número entre 1 y " + N + ", adivina cual es:");
+			respuesta = scan.nextInt();
+			
+			if(respuesta != numeroAdivinar) {
+				System.out.println("Lo siento no has acertado, sigue jugando");
+			}else {
+				System.out.println("Enhorabuena!!!Has acertado!!!!");
+			}
+		} while (respuesta != numeroAdivinar);
+		
+		scan.close();
+	}
+
+}
