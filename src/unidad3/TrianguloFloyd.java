042987338745de3dc45f2aa99c4c9794147ac0f7
Andrea <andreagutierrezvega@gmail.com>
2019-11-21 21:10:45 +0100

    Ejercicio 9 de la unidad 3 resuelto

---
+++ src/unidad3/TrianguloFloyd.java
@@ -0,0 +1,24 @@
+package unidad3;
+import java.util.Scanner;
+
+public class TrianguloFloyd {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int filas;
+		int numImprimir=1;
+		System.out.println("Introduce un númer de filas para generar el triángulo de Floyd");
+		filas = scan.nextInt();
+				
+		for(int i=1;i<=filas;i++) {
+			for(int j=1; j <= i; j++) {
+				System.out.print(numImprimir + "\t" );
+				numImprimir++;
+			}
+			System.out.println();
+		}
+		
+		scan.close();
+	}
+
+}
